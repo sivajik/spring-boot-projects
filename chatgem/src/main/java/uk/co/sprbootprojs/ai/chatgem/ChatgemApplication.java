8d4be1ff2da4fe42ed86e7f56d728f2061a5fa90
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-02-21 15:41:15 +0000

    first commit of gemini chat app

---
+++ chatgem/src/main/java/uk/co/sprbootprojs/ai/chatgem/ChatgemApplication.java
@@ -0,0 +1,13 @@
+package uk.co.sprbootprojs.ai.chatgem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgemApplication.class, args);
+	}
+
+}
---
+++ chatgem/src/test/java/uk/co/sprbootprojs/ai/chatgem/ChatgemApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.sprbootprojs.ai.chatgem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
