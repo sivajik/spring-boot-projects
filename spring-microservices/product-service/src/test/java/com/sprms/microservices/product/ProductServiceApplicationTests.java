ba5638e8d1d7e0ae16b5469de69c7a49978b65ed
sivajik <sivaji_kondapalli@yahoo.com>
2025-05-02 13:52:33 +0100

    added integration tests for product service

--- spring-microservices/product-service/src/test/java/com/sprms/microservices/product/ProductServiceApplicationTests.java
+++ spring-microservices/product-service/src/test/java/com/sprms/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,54 @@
 package com.sprms.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name": "iphone15",
+                    "description": "iphone15 is from apple",
+                    "price": 850
+                }
+                """;
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("iphone15"))
+                .body("description", Matchers.equalTo("iphone15 is from apple"))
+                .body("price", Matchers.equalTo(850));
+
+    }
 
 }
